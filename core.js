86bced924871cb23da90a24e9fe88be5fad788b9
dovhart <106178240+dovhart@users.noreply.github.com>
2024-03-12 11:00:10 -0700

    Add files via upload

--- core.js
+++ core.js
@@ -224,9 +224,7 @@ async function updatePage() {
   const fileVersion = document
     .querySelector(".version")
     .getAttribute("content");
-  const response = await fetch(
-    "/wcwr/version.json?" + 1 * Math.random().toString().split(".")[1]
-  );
+  const response = await fetch("/wcwr/version.json?" + Date.now());
   const version = await response.json();
   if (fileVersion < version.pages[PAGE]) {
     location.reload(true);
